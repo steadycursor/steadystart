a0017cd06363c01423cfd7a1cb2fdd94bc5c63d9
Petr Homoky <p.homoky@gmail.com>
2025-02-02 20:11:22 +0000

    [ui] fresh nextjs app

---
+++ apps/ui/src/pages/index.tsx
@@ -0,0 +1,21 @@
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <div
+      className={`${geistSans.variable} ${geistMono.variable} font-[family-name:var(--font-geist-sans)]`}
+    > 
+   Ahoj
+    </div>
+  );
+}
