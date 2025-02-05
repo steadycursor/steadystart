c039572bafa33e4643e2704fbc04b52731c455de
Petr Homoky <p.homoky@gmail.com>
2025-02-05 21:32:54 +0000

    [ui] styled FieldWrapper with field error

--- apps/ui/src/components/FieldWrapper.tsx
+++ apps/ui/src/components/FieldWrapper.tsx
@@ -2,17 +2,20 @@ import { ClassNameProps } from '@/types/ClassNameProps';
 import { ChildrenProps } from '@/types/ChildrenProps';
 import { Div } from './Div';
 import { Label, LabelProps } from './Label';
+import { InputControlError } from './InputControlError';
 
 type FieldWrapperProps = ChildrenProps &
   ClassNameProps & {
     label: LabelProps;
+    error?: { message?: string };
   };
 
-export const FieldWrapper = ({ label, className, children }: FieldWrapperProps) => {
+export const FieldWrapper = ({ label, error, className, children }: FieldWrapperProps) => {
   return (
-    <Div className={[className]}>
+    <Div className={[className, 'space-y-1.5']}>
       <Label {...label} />
-      <Div className={['mt-2']}>{children}</Div>
+      <Div>{children}</Div>
+      {error && <Div className="text-sm text-red-600">{error.message}</Div>}
     </Div>
   );
 };
