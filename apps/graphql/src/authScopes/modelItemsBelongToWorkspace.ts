783698b6ff0d55b12372934e0806fe4b54d9682d
Petr Homoky <p.homoky@gmail.com>
2025-04-04 20:32:59 +0000

    [graphql] add documentation note about required test cases when extending Model type

--- apps/graphql/src/authScopes/modelItemsBelongToWorkspace.ts
+++ apps/graphql/src/authScopes/modelItemsBelongToWorkspace.ts
@@ -1,5 +1,12 @@
 import { Context } from '../context';
 
+/**
+ * IMPORTANT NOTES FOR CLAUDE:
+ * - When adding a new model ype to the Model union create for new model test in modelItemsBelongToWorkspace.test.ts
+ *    - Should work when {{model}} belongs to workspace with id argument
+ *    - Should not work when {{model}} does not belong to workspace
+ */
+
 export type Model = 'post';
 
 export type ModelItemsBelongToWorkspaceScopeArgs = ({ id: string } | { ids: string[] }) & { model: Model };
