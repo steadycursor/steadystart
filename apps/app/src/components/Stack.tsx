b3f6a4c04436d632462042024c5791dcbbd0a533
Petr Homoky <p.homoky@gmail.com>
2025-04-06 13:08:42 +0000

    [component] enhance Stack component with direction prop

--- apps/app/src/components/Stack.tsx
+++ apps/app/src/components/Stack.tsx
@@ -1,13 +1,17 @@
+import { FC } from 'react';
 import { Div } from './Div';
 import { ChildrenProps } from '../types/ChildrenProps';
 import { ClassNameProps } from '../types/ClassNameProps';
 
-type StackProps = ChildrenProps & ClassNameProps;
+type StackProps = ChildrenProps & ClassNameProps & {
+  direction?: "column" | "row"
+};
 
-export const Stack = ({ children, className }: StackProps) => (
+export const Stack: FC<StackProps> = ({ direction, children, className }) => (
   <Div
     className={[
-      'flex flex-col gap-3', //
+      'flex gap-3', //
+      direction !== "row" && "flex-col",
       className,
     ]}
   >
