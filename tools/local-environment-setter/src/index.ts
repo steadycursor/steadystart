631d4f7c4445ef59386f04ce5aa99452af290c27
Petr Homoky <p.homoky@gmail.com>
2025-05-08 19:08:01 +0000

    [tools] add local-environment-setter utility for switching between environments

---
+++ tools/local-environment-setter/src/index.ts
@@ -0,0 +1,66 @@
+import { prompt } from 'enquirer';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const constants = {
+  stage: {
+    DATABASE_URL: '',
+    DATABASE_DIRECT_URL: '',
+  },
+  production: {
+    DATABASE_URL: '',
+    DATABASE_DIRECT_URL: '',
+  },
+  local: {
+    DATABASE_URL: 'postgresql://postgres:mysecretpassword@localhost:5432/initdb&connection_limit=1&pool_timeout=30',
+    DATABASE_DIRECT_URL: 'postgresql://postgres:mysecretpassword@localhost:5432/initdb&connection_limit=1&pool_timeout=30',
+  },
+} as const;
+
+(async () => {
+  const envFilePath = path.resolve(__dirname, '../../../.env');
+
+  let envConfig = fs.readFileSync(envFilePath, 'utf-8');
+
+  console.info('\nCurrent environment:', envConfig.match(/ENVIRONMENT=(.*)/)?.[0].replace('ENVIRONMENT=', ''), '\n');
+
+  const { environment }: { environment: 'stage' | 'production' } = await prompt({
+    type: 'autocomplete',
+    name: 'environment',
+    message: 'Select desired environment:',
+    choices: [
+      { value: 'stage', name: 'stage' },
+      { value: 'production', name: 'production' },
+    ],
+  });
+
+  const { shouldUseLocalDatabaseUrl }: { shouldUseLocalDatabaseUrl: boolean } = await prompt({
+    type: 'confirm',
+    name: 'shouldUseLocalDatabaseUrl',
+    message: 'Do you want to use local DB?',
+  });
+
+  if (environment === 'stage') {
+    envConfig = envConfig.replace(/ENVIRONMENT=.*/, 'ENVIRONMENT=stage');
+    envConfig = envConfig.replace(/DATABASE_URL=.*/, `DATABASE_URL=${constants.stage.DATABASE_URL}`);
+    envConfig = envConfig.replace(/DATABASE_DIRECT_URL=.*/, `DATABASE_DIRECT_URL=${constants.stage.DATABASE_DIRECT_URL}`);
+  }
+
+  if (environment === 'production') {
+    envConfig = envConfig.replace(/ENVIRONMENT=.*/, 'ENVIRONMENT=production');
+    envConfig = envConfig.replace(/DATABASE_URL=.*/, `DATABASE_URL=${constants.production.DATABASE_URL}`);
+    envConfig = envConfig.replace(/DATABASE_DIRECT_URL=.*/, `DATABASE_DIRECT_URL=${constants.production.DATABASE_DIRECT_URL}`);
+  }
+
+  if (shouldUseLocalDatabaseUrl) {
+    envConfig = envConfig.replace(/DATABASE_URL=.*/, `DATABASE_URL=${constants.local.DATABASE_URL}`);
+    envConfig = envConfig.replace(/DATABASE_DIRECT_URL=.*/, `DATABASE_DIRECT_URL=${constants.local.DATABASE_DIRECT_URL}`);
+  }
+
+  console.info('\nNew values:\n');
+  console.info(envConfig.match(/ENVIRONMENT=(.*)/)?.[0], '\n');
+  console.info(envConfig.match(/DATABASE_URL=(.*)/)?.[0], '\n');
+  console.info(envConfig.match(/DATABASE_DIRECT_URL=(.*)/)?.[0], '\n');
+
+  fs.writeFileSync(envFilePath, envConfig);
+})();
