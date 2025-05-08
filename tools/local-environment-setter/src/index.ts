968e3ac5d89d959b814a526c4aa4a9a382fdfbde
Petr Homoky <p.homoky@gmail.com>
2025-05-08 19:22:31 +0000

    [tools] add Clerk authentication keys to environment setter

--- tools/local-environment-setter/src/index.ts
+++ tools/local-environment-setter/src/index.ts
@@ -6,10 +6,14 @@ const constants = {
   stage: {
     DATABASE_URL: '',
     DATABASE_DIRECT_URL: '',
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: '',
+    CLERK_SECRET_KEY: '',
   },
   production: {
     DATABASE_URL: '',
     DATABASE_DIRECT_URL: '',
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: '',
+    CLERK_SECRET_KEY: '',
   },
   local: {
     DATABASE_URL: 'postgresql://postgres:mysecretpassword@localhost:5432/initdb&connection_limit=1&pool_timeout=30',
@@ -44,12 +48,22 @@ const constants = {
     envConfig = envConfig.replace(/ENVIRONMENT=.*/, 'ENVIRONMENT=stage');
     envConfig = envConfig.replace(/DATABASE_URL=.*/, `DATABASE_URL=${constants.stage.DATABASE_URL}`);
     envConfig = envConfig.replace(/DATABASE_DIRECT_URL=.*/, `DATABASE_DIRECT_URL=${constants.stage.DATABASE_DIRECT_URL}`);
+    envConfig = envConfig.replace(
+      /NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=.*/,
+      `NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=${constants.stage.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}`,
+    );
+    envConfig = envConfig.replace(/CLERK_SECRET_KEY=.*/, `CLERK_SECRET_KEY=${constants.stage.CLERK_SECRET_KEY}`);
   }
 
   if (environment === 'production') {
     envConfig = envConfig.replace(/ENVIRONMENT=.*/, 'ENVIRONMENT=production');
     envConfig = envConfig.replace(/DATABASE_URL=.*/, `DATABASE_URL=${constants.production.DATABASE_URL}`);
     envConfig = envConfig.replace(/DATABASE_DIRECT_URL=.*/, `DATABASE_DIRECT_URL=${constants.production.DATABASE_DIRECT_URL}`);
+    envConfig = envConfig.replace(
+      /NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=.*/,
+      `NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=${constants.production.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}`,
+    );
+    envConfig = envConfig.replace(/CLERK_SECRET_KEY=.*/, `CLERK_SECRET_KEY=${constants.production.CLERK_SECRET_KEY}`);
   }
 
   if (shouldUseLocalDatabaseUrl) {
